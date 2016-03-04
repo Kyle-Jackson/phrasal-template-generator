9a7a5524fd6c2ef09f2033231a53288ff483e6c7
Kyle-Jackson <kylejackson@clare-pc.gateway.2wire.net>
2016-03-03 21:29:15 -0500

    Fixed indentation error.

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\nEnter the letter below:');
 
-if (story == '1') {
+if (story == 'a') {
   document.write(adjective + ' After snacking on treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
 }
 
-if (story == '2') {
+if (story == 'b') {
   document.write(' Ameilia ' + verb + ' through the ' + adjective + ' ' + color + ' nebula escaping the space ' + creature + '.');
 }
 
-if (story == '3') {
+if (story == 'c') {
   document.write(' Jackson chose his ' + adjective + ' , ' + colour + ' ' + creature + ' card and ' + verb + ' it to the table knowing he won. ');
 }
